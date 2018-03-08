c14a6cac31f8e18b32f3d87ee8458fc678028a36
Matthias Holzer <ma.holzer@syskron.com>
2018-03-08 14:27:57 +0100

    add router to to breadcrumb

--- readable-app/src/App.js
+++ readable-app/src/App.js
@@ -1,44 +1,36 @@
 import * as React from 'react';
-import logo from './logo.svg';
-import './css/App.css';
-import './css/bootstrap.min.css';
-import {Breadcrumb, BreadcrumbItem} from 'react-bootstrap/lib';
+import 'css/App.css';
+import 'css/bootstrap.min.css';
+
 import PropTypes from 'prop-types';
+
 import { Link, Route, Redirect, Switch } from 'react-router-dom'
-import NotFound from './components/NotFound'
-import FirstGlance from './components/firstGlance'
 
-
+import NotFound from 'components/NotFound'
+import FirstGlance from 'components/firstGlance'
+import NavigationBar from 'components/navigation-bar'
+import Header from 'components/header'
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Readable</h1>
-        </header>
-        <Breadcrumb>
-            <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-            <Breadcrumb.Item href="/category">
-              Library
-          </Breadcrumb.Item>
-            <Breadcrumb.Item active>Data</Breadcrumb.Item>
-          </Breadcrumb>
+        <Header />
+        <NavigationBar />
 
-          <Switch>
+        <Switch>
           <Route
             exact path='/'
-            render={(props) =>(
-                <FirstGlance/>
-              )}
+            render={(props) => (
+              <FirstGlance />
+            )}
           />
           <Route
-          path='/404'
-          render={(props) =>(
-            <NotFound/>
-          )}/>
+            path='/404'
+            render={(props) => (
+              <NotFound />
+            )} />
           <Redirect from='*' to='/404' />
         </Switch>
 
--- readable-app/src/components/navigation-bar/index.js
+++ readable-app/src/components/navigation-bar/index.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-
+import { Breadcrumb, BreadcrumbItem } from 'react-bootstrap/lib';
+import { Link, Route, Redirect, Switch } from 'react-router-dom'
 
 class NavigationBar extends Component {
     render() {
         return (
-            <Breadcrumb>
-            <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-            <Breadcrumb.Item href="/category">
-              Library
-          </Breadcrumb.Item>
-            <Breadcrumb.Item active>Data</Breadcrumb.Item>
+          <Breadcrumb>
+            <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
+            <Breadcrumb.Item><Link to="/404">404</Link></Breadcrumb.Item>
+            <Breadcrumb.Item><Link to="/sxdfvgzbjhi">bullshit</Link></Breadcrumb.Item>
           </Breadcrumb>
         );
     }
